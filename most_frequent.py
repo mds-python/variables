e57fabb7b1f703595b03ce66da2ca43f625aaf3c
Maciej Dems <maciej.dems@p.lodz.pl>
2020-10-22 10:24:41 +0200

    Exercise template

---
+++ most_frequent.py
@@ -0,0 +1,22 @@
+﻿"""
+Please write a program that reads a line of text, and then prints the most frequent letter in it. The program should ignore case,
+and the most frequent letter should be capitalized. If there is more than one letter that appears most frequently, it is enough
+to print any of them. (for those of you who are ambitious: in such case, write down all the letters that appear most of the times,
+each in a separate line)
+
+Example:
+
+    Write the text: To be or not to be
+    O
+
+In the advanced version:
+
+    Enter text: Abba
+    A
+    B
+
+Tip: I suggest creating a dictionary that stores for each letter the number of its occurrences. The number of occurrences of
+a given letter can be found using the `count` method for a text string (please search for its documentation yourself).
+"""
+
+# Your code here
